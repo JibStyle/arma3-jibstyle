b552edcb3be5f2708508a0781837fd7da2546415
JibStyle <jibstyle209@gmail.com>
2023-01-21 01:09:16 -0800

    Add emitter addon

---
+++ addons/emitter/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+    class jib_emitter {
+        name = "Jib Emitter";
+        author = "JibStyle";
+        requiredVersion = 1.60;
+        requiredAddons[] = {};
+        units[] = {
+        };
+    };
+};
+
+class CfgFunctions {
+    class jib_emitter {
+        class jib_emitter {
+            file = "x\jib\addons\emitter";
+            class emitter { preInit = 1; recompile = 1; };
+        };
+    };
+};
