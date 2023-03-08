0bab1172b5b1becd200088fcddfca437ea6c6963
JibStyle <jibstyle209@gmail.com>
2023-03-07 21:32:52 -0800

    Remove old service files

--- addons/service/config.cpp
+++ addons/service/config.cpp
@@ -12,24 +12,8 @@ class CfgFunctions {
     class jib_service {
         class jib_service {
             file = "x\jib\addons\service";
-
             class service {
                 recompile = 1; preInit = 1;
-            };
-            class canServiceVehicle {
-                recompile = 1;
-            };
-            class setupObjectServiceAI {
-                recompile = 1;
-            };
-            class setupObjectServicePlayer {
-                recompile = 1;
-            };
-            class setupObjectServiceVehicle {
-                recompile = 1;
-            };
-            class setupPlayerServiceVehicle {
-                recompile = 1;
             };
         };
     };
